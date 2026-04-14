b2002d291b533119ae01dea5c8d190c8b03cc99d
belladonna.giacomo <belladonna.giacomo@HP-003>
2026-04-14 10:08:32 +0200

    mod

--- Liste2/Program.cs
+++ Liste2/Program.cs
@@ -2,9 +2,24 @@
 {
     internal class Program
     {
+        static List<int> EliminaI(List<int> lista1)
+        {
+            for(int i = lista1.Count - 1; i >= 0; i--)
+            {
+                if (lista1[i] == i)
+                {
+                    lista1.RemoveAt(i);
+                }
+            }
+            return lista1;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> lista1 = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
+            foreach(int i in EliminaI(lista1))
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
