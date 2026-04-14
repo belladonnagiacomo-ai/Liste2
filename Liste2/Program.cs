8f98f775abcc8e3b0ca88a6d76b8cf645c7bffe0
belladonna.giacomo <belladonna.giacomo@HP-003>
2026-04-14 10:20:36 +0200

    mod

--- Liste2/Program.cs
+++ Liste2/Program.cs
@@ -13,6 +13,29 @@
             }
             return lista1;
         }
+        static void Spostamenti(List<int> numeri, int s)
+        {
+            int somma;
+            for(int i = 0;  i < numeri.Count; i++)
+            {
+                somma = 0;
+                somma = i + s;
+                if(somma >= numeri.Count)
+                {
+                    somma = somma - numeri.Count;
+                    numeri[i] = numeri[somma];
+                }
+                else
+                {
+                    numeri[i] = numeri[somma];
+                }
+                
+            }
+            foreach(int i in numeri)
+            {
+                Console.WriteLine(i);
+            }
+        }
         static void Main(string[] args)
         {
             List<int> lista1 = new List<int>() { 0, 5, 2, 8, 4, 10, 6, 7 };
@@ -20,6 +43,13 @@
             {
                 Console.WriteLine(i);
             }
+            Console.WriteLine();
+
+            List<int> numeri = new List<int>() { 1, 2, 3, 4, 5 };
+            Console.WriteLine("Dammi il numero di spostamenti");
+            int s = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+            Spostamenti(numeri, s);
         }
     }
 }
